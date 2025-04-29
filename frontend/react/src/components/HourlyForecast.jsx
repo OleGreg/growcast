c08dcba1c854a0ae55fbf157af3071b961124594
Greg Crowell <Greg4510@gmail.com>
2025-04-28 22:19:19 -0400

    prep for tailwind

--- frontend/react/src/components/HourlyForecast.jsx
+++ frontend/react/src/components/HourlyForecast.jsx
@@ -5,20 +5,22 @@ const HourlyForecast = ({ hourly }) => {
   // Only show the next 12 hours
   const hoursToDisplay = hourly.slice(2, 12);
 
+  const hourly_forecasts = hoursToDisplay.map((hour, index) => {
+    const date = new Date(hour.dt * 1000); // Convert UNIX timestamp to ms
+    const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    return (
+      <li key={index} className="hour-card">
+        <p><strong>{time}</strong></p>
+        <p>{Math.round(hour.temp)}°</p>
+        <p>{hour.weather[0].main}</p>
+      </li>
+    );
+  })
+
   return (
     <ul className="hourly-forecast">
-      {hoursToDisplay.map((hour, index) => {
-        const date = new Date(hour.dt * 1000); // Convert UNIX timestamp to ms
-        const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-        return (
-          <li key={index} className="hour-card">
-            <p><strong>{time}</strong></p>
-            <p>{Math.round(hour.temp)}°</p>
-            <p>{hour.weather[0].main}</p>
-          </li>
-        );
-      })}
+      {hourly_forecasts}
     </ul>
   );
 };
