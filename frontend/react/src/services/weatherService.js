9589dace72c821dba6bb5d757caf6270b7cd1727
Greg Crowell <Greg4510@gmail.com>
2025-05-05 15:18:48 -0400

    add prod host to nginx.conf

--- frontend/react/src/services/weatherService.js
+++ frontend/react/src/services/weatherService.js
@@ -1,6 +1,8 @@
 // src/services/weatherService.js
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function fetchWeather() {
-  const response = await fetch('http://api.growcast.local/weather');
+  const response = await fetch(`${API_URL}/weather`);
   if (!response.ok) {
     throw new Error('Network Error');
   }
